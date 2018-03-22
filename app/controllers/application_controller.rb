d8f30149d14f376ea2c02c097df8371db6e1a956
jbag123 <joshcone@googlemail.com>
2018-03-22 20:38:22 +0000

    commiting user story 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery prepend: true
   before_action :authenticate_user!
-  after_initialize :init
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -13,5 +12,4 @@ class ApplicationController < ActionController::Base
     redirect_to(request.referrer || root_path)
   end
 
-  enum role: [:standard, :premium, :admin]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_initialize :init
+
+  def init
+   self.role ||= :standard
+  end
+
+  enum role: [:standard, :premium, :admin]
 end
