4a81e7fc4644ce31125c916866b3bf81b0cb60c2
jbag123 <joshcone@googlemail.com>
2018-04-13 10:59:06 +0100

    commiting single db query for downgrade and warning message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   def downgrade
     current_user.update_attribute(:role, 'standard')
     @user = User.find(params[:id])
-    @user.wikis.each do |w|
-      w.update_attributes(private: false)
-    end
+    @user.wikis.update_all(private: false)
     flash[:notice] = "#{current_user.email} you're account has been downgraded"
     redirect_to edit_user_registration_path
   end
