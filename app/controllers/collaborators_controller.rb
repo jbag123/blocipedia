bf43d6bc98b7363809ccde878940fb9b0df30266
jbag123 <joshcone@googlemail.com>
2018-04-15 18:04:26 +0100

    commiting resolution of collaboration issues

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -3,11 +3,11 @@ class CollaboratorsController < ApplicationController
     @wiki = Wiki.find(params[:wiki_id])
     @user = User.find_by_email(params[:collaborator][:user])
     @collaborator = @wiki.collaborators.new(wiki_id: @wiki.id, user_id: @user.id)
-    if @collaborator.save
+    if @wiki.collaborators.exists?(user_id: @user.id)
+      flash[:alert] = "Collaborator already exists"
+    elsif @collaborator.save
       flash[:notice] = "Collaborator added"
-    else
-      flash[:error] = "There was an error"
     end
-    redirect_to root_path
+    redirect_to @wiki
   end
 end
